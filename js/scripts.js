333518ca007f2a2a551890e52b51fa2161fbf309
Student <example@example.com>
2020-02-18 18:51:28 -0800

    add initial user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 console.log("Hello world");
+
+
+
+
+
+
+// user interface logic
+$(document).ready(function() {
+    $("form#register").submit(function(event) {
+        event.preventDefault();
+        var name = $("#full-name").val();
+        var initialDeposit = $("#initial-deposit").val();
+        var deposit = $("#deposit").val();
+        var withdrawal = $("#withdraw").val();
+        // console.log(name, initialDeposit, deposit, withdrawal);
+    })
+// 
+});
