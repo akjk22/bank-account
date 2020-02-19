804f458f4015a53f53674e25dfa12ffb41b66962
Student <example@example.com>
2020-02-18 20:18:08 -0800

    fix bug that was excluding initial deposit from the current balance

--- js/scripts.js
+++ js/scripts.js
@@ -10,20 +10,24 @@ function bankAccount(name, initialDeposit, deposit, withdrawal, balance) {
 
 };
 
-bankAccount.prototype.register = function() {
-    return "You're registered " + this.name;
-};
+bankAccount.prototype.initializeBalance = function() {
+    return this.balance = this.initialDeposit;
+}
 
 bankAccount.prototype.addDeposit = function() {
     return this.balance = parseInt(this.balance) + parseInt(this.deposit);
 };
 
 bankAccount.prototype.subtractWithdraw = function() {
-    
+    return this.balance = parseInt(this.balance) - parseInt(this.withdrawal);
 };
 
 bankAccount.prototype.showBalance = function() {
-    return "This is your current balanace " + this.balance;
+    // return this.balance;
+    // return "This is your current balance " + this.balance;
+    // this.balance = this.initialDeposit;
+    return this.balance;
+    // return this.initialDeposit;
 };
 
 
@@ -32,16 +36,23 @@ bankAccount.prototype.showBalance = function() {
 $(document).ready(function() {
     $("form#register").submit(function(event) {
         event.preventDefault();
-        var name = $("#full-name").val();
+        var name = "" + $("#full-name").val();
         var initialDeposit = $("#initial-deposit").val();
         var deposit = $("#deposit").val();
         var withdrawal = $("#withdraw").val();
         var balance = 0.00;
         
         var bankAccount1 = new bankAccount(name, initialDeposit, deposit, withdrawal, balance)
-        console.log("This our first customers name on his bank account " + bankAccount1.name);
-        console.log(bankAccount1.register());
-        console.log(bankAccount1.addDeposit());
-        console.log(bankAccount1.showBalance());
-    })
+        bankAccount1.initializeBalance();
+        bankAccount1.addDeposit();
+        bankAccount1.subtractWithdraw();
+
+        // console.log(bankAccount1.showBalance());
+        // console.log("This our first customers name on his bank account " + bankAccount1.name);
+        // console.log(bankAccount1.initializeBalance());
+        // console.log(bankAccount1.addDeposit());
+        // console.log(bankAccount1.subtractWithdraw());
+        // console.log(bankAccount1.showBalance());
+        $("#show-balance").text("Your current balance is " + bankAccount1.showBalance());
+    });
 });
